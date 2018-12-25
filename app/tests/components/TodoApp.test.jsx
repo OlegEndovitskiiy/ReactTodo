df609f421be0964bcfff998b8f9a84d838db4eae
Endovitsky <o.endovitsky@itransition.corp>
2018-12-25 11:11:09 +0300

    Add completed and created timestamps

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -4,6 +4,7 @@ var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 var TodoApp = require('TodoApp');
+var moment = require('moment');
 
 describe('TodoApp', () => {
     it('should exist', () => {
@@ -18,13 +19,16 @@ describe('TodoApp', () => {
         todoApp.handleAddTodo(todoText);
 
         expect(todoApp.state.todos[0].text).toBe(todoText);
+        expect(todoApp.state.todos[0].createdAt).toBeA('number');
     });
 
     it('should toggle completed value when handleTogle called', () => {
         var todoData = {
             id: 11,
             text: 'Test features',
-            completed: false
+            completed: false,
+            createdAt: 0,
+            completedAt: undefined
         };
         var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
         todoApp.setState({
@@ -34,5 +38,26 @@ describe('TodoApp', () => {
         expect(todoApp.state.todos[0].completed).toBe(false);
         todoApp.handleToggle(todoData.id);
         expect(todoApp.state.todos[0].completed).toBe(true);
+        expect(todoApp.state.todos[0].completedAt).toBeA('number');
+    });
+
+    // Test that when toggle from true to false, completedAt get removed
+    it('should toggle todo from completed to incompleted', () => {
+        var todoData = {
+            id: 11,
+            text: 'Test features',
+            completed: true,
+            createdAt: 0,
+            completedAt: 123
+        };
+        var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+        todoApp.setState({
+            todos: [todoData]
+        });
+
+        expect(todoApp.state.todos[0].completed).toBe(true);
+        todoApp.handleToggle(todoData.id);
+        expect(todoApp.state.todos[0].completed).toBe(false);
+        expect(todoApp.state.todos[0].completedAt).toNotExist();
     });
 });
